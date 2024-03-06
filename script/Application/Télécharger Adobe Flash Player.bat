79e0b0440ebb92f4934457d13d2e42f2d1c118f2
Le Bazar de Bryan <85786075+LeBazarDeBryan@users.noreply.github.com>
2024-03-06 19:23:17 +0000

    Update

--- script/Application/Télécharger Adobe Flash Player.bat
+++
@@ -1,2 +0,0 @@
-mode con: cols=16 lines=1
-start https://web.archive.org/web/20200523210841/http://fpdownload.adobe.com/get/flashplayer/pdc/32.0.0.371/install_flash_player.exe
--- script/Utile/Chris Titus Tech Windows Utility.ps1
+++
@@ -1,9 +0,0 @@
-if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-        $Command = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-        Start-Process -FilePath PowerShell.exe -Verb RunAs -ArgumentList $Command
-        Exit
- }
-}
-
-[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12;iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/ChrisTitusTech/winutil/main/winutil.ps1')
